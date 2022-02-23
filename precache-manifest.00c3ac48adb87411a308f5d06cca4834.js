c80ff044fdc444c5e9d6ef5d9a5b8c3b562971ab
Lucas Bollella <lubolle@gmail.com>
2022-02-23 00:13:10 -0300

    Updates

---
+++ precache-manifest.00c3ac48adb87411a308f5d06cca4834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b5d1e4d418802dc310cda80f9daa62",
+    "url": "/buscadordegatos/index.html"
+  },
+  {
+    "revision": "ff8a8d053c5ae29881dd",
+    "url": "/buscadordegatos/static/css/main.3edd6c0e.chunk.css"
+  },
+  {
+    "revision": "42d8e8859104679ce821",
+    "url": "/buscadordegatos/static/js/2.bc0e5e72.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/buscadordegatos/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "ff8a8d053c5ae29881dd",
+    "url": "/buscadordegatos/static/js/main.548fb4a3.chunk.js"
+  },
+  {
+    "revision": "424dbabf41451bd2711c",
+    "url": "/buscadordegatos/static/js/runtime~main.61135fcc.js"
+  }
+]);
